6cfdfe09242932983acef7b731a4d5249f975399
szylem <62067049+szylem@users.noreply.github.com>
2020-03-14 18:08:40 +0100

    Create pokaz.js

---
+++ pokaz.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://saint:praca@cluster0-iip04.mongodb.net/test?retryWrites=true&w=majority"
+
+exports.pokazSprawy = function (res, q, qdata) {
+  console.log('Tablica');
+  MongoClient.connect(uri, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("saint");
+    var query = { aktywny: "1" };
+    dbo.collection("dash").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+};
