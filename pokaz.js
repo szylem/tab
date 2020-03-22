f33ebd955a823b84436372149742d53c5039c863
szylem <62067049+szylem@users.noreply.github.com>
2020-03-22 11:54:50 +0100

    Update pokaz.js

--- pokaz.js
+++ pokaz.js
@@ -3,7 +3,7 @@ const uri = "mongodb+srv://saint:praca@cluster0-iip04.mongodb.net/test?retryWrit
 
 
 exports.pokazSprawy = function (res, q, qdata) {
-  var wynik = [];
+  
   console.log('Tablica');
   MongoClient.connect(uri, function(err, db) {
     if (err) throw err;
@@ -11,12 +11,13 @@ exports.pokazSprawy = function (res, q, qdata) {
     var query = { aktywny: 1 };
     dbo.collection("dash").find(query).toArray(function(err, result) {
       if (err) throw err;
+        var wynik = [];  
         console.log(result[0].obiekt);  
         wynik.push(result[0].obiekt);
-      db.close();
+        db.close();
     });
     
   });
-  console.log(wynik[0]);
+  console.log(wynik.length);
   //printjson (wynik[0]);
 };
