fa722f84697c949c88408d51dd83c8f4f7674ecc
szylem <62067049+szylem@users.noreply.github.com>
2020-03-22 11:09:15 +0100

    Update pokaz.js

--- pokaz.js
+++ pokaz.js
@@ -11,14 +11,10 @@ exports.pokazSprawy = function (res, q, qdata) {
     var query = { aktywny: 1 };
     wynik = dbo.collection("dash").find(query).toArray(function(err, result) {
       if (err) throw err;
-      //console.log(result);
-      for (x in result) {
-        //console.log(result[x].obiekt);
-        //console.log(result[x].msg);
-      } 
+      wynik.forEach( function(myRes) { console.log( "user: " + myRes.obiekt ); } ); 
       db.close();
     });
-    console.log(printjson (wynik[0]));
+    
   });
-  //res.write(wynik[0].msg);
+  
 };
