12a7d61e7c580abf23e87fad852f2eb75ecfe60f
szylem <62067049+szylem@users.noreply.github.com>
2020-03-22 23:07:19 +0100

    Update pokaz.js

--- pokaz.js
+++ pokaz.js
@@ -3,12 +3,13 @@ const uri = "mongodb+srv://saint:praca@cluster0-iip04.mongodb.net/test?retryWrit
 
 
 exports.pokazSprawy = function (res, q, qdata) {
+  var wynik;
   console.log('Tablica');
   MongoClient.connect(uri, function(err, db) {
     if (err) throw err;
     var dbo = db.db("saint");
     var query = { aktywny: 1 };
-    var wynik = dbo.collection("dash").find(query).toArray(function(err, result) {
+    wynik = dbo.collection("dash").find(query).toArray(function(err, result) {
       if (err) throw err;
       console.log(wynik.length); 
         db.close();
