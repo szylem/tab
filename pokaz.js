a76b9f18ea9a72fc61b3d2a92db343fa02452eee
szylem <62067049+szylem@users.noreply.github.com>
2020-03-23 01:12:05 +0100

    Update pokaz.js

--- pokaz.js
+++ pokaz.js
@@ -4,20 +4,24 @@ var wynik;
 
 exports.pokazSprawy = function (res, q, qdata) {
   console.log('Tablica');
+  var resultArray = [];
   MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
-    if (err) throw err;
+      if (err) throw err;
       var dbo = client.db("saint");
       var query = { aktywny: 1 };
-      dbo.collection("dash").find(query).then((docs) => {
-        console.log(docs);
-        
-      }).catch((err) => {
-        console.log(err);
-      }).finally(() => {
-        client.close();
+      var cursor = dbo.collection("dash").find(query);
+      cursor.forEach(function(doc, err){
+        if (err) throw err;
+        resultArray.push(doc);
+      }, function(){
+      client.close();
+      //I have no index file to render, so I print the result to console
+      //Also send back the JSON string bare through the channel
+      console.log(resultArray);
+      //res.send(resultArray);
       });
   });
   
-  console.log(wynik + "  poza");  
+  //console.log(wynik + "  poza");  
   
 };
