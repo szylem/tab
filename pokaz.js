a97c192bf47085c5033616f568fb0f41910007e8
szylem <62067049+szylem@users.noreply.github.com>
2020-03-24 23:00:31 +0100

    Update pokaz.js

--- pokaz.js
+++ pokaz.js
@@ -13,7 +13,8 @@ exports.pokazSprawy = function (res, q, qdata) {
       dbo.collection("dash").find(query).toArray(function(err, result) {
           if (err) throw err;
           console.log("Wyniki z bazy");
-          wyniki[0] = result[0].obiekt;
+          console.log(result[0].obiekt);
+          wyniki = result.slice();
           client.close();
       });
   });
