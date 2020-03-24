0520086f0d7c9c4072a2b63c797e84728547f790
szylem <62067049+szylem@users.noreply.github.com>
2020-03-24 22:48:27 +0100

    Update pokaz.js

--- pokaz.js
+++ pokaz.js
@@ -1,28 +1,23 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://saint:praca@cluster0-iip04.mongodb.net/test?retryWrites=true&w=majority"
-var wynik;
+
 
 exports.pokazSprawy = function (res, q, qdata) {
   console.log('Tablica');
-  var resultArray = [];
+  
   MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
       if (err) throw err;
       var dbo = client.db("saint");
       var query = { aktywny: 1 };
       var cursor = dbo.collection("dash").find(query);
-      cursor.forEach(function(doc, err){
-        if (err) throw err;
-        resultArray.push(doc);
-      }, function(){
-      client.close();
-      //I have no index file to render, so I print the result to console
-      //Also send back the JSON string bare through the channel
-      //console.log(resultArray);
-      //res.send(resultArray);
+      dbo.collection("dash").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          console.log("Wyniki z bazy");
+          db.close();
       });
   });
   
-  while(resultArray.lenght == undefined) { console.log("czekam.."); };
-  console.log(resultArray.lenght);  
+  
+  console.log("Połączenie zakończone");  
   
 };
